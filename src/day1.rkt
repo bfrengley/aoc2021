1dff9d25abcf3ea8f9a833ed8821be6c7a110c69
Ben Frengley <ben.frengley@gmail.com>
2021-12-02 20:04:03 +1300

    Day 1

---
+++ src/day1.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(require "util.rkt")
+
+(define (parse in) (stream-map string->number (sequence->stream(in-lines in))))
+
+(define (part-1)
+  (with-day 1
+    (λ (in)
+      (let* ([depths (parse in)]
+             [pairs (stream-zip depths (stream-rest depths))])
+        (stream-count (λ (ds) (apply < ds)) pairs)))))
+
+(define (part-2)
+  (with-day 1
+    (λ (in)
+      (let* ([depths (parse in)]
+             [pairs (stream-zip depths (stream-tail depths 3))])
+        (stream-count (λ (ds) (apply < ds)) pairs)))))
+
+(module* main #f
+  (printf "Part 1: ~a\n" (part-1))
+  (printf "Part 2: ~a\n" (part-2)))
---
+++ src/util.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(provide with-day zip stream-zip)
+
+(require racket/file)
+
+(define (with-day day fn)
+  (let* ([filename (string-append "day" (number->string day) ".txt")]
+         [filepath (build-path (current-directory) "input" filename)])
+    (call-with-input-file filepath fn)))
+
+(define (zip as bs)
+  (for/list ([a as]
+             [b bs])
+    (list a b)))
+
+(define (stream-zip as bs)
+  (for/stream ([a as]
+               [b bs])
+    (list a b)))
